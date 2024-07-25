2d2d6ded3887798f8f4e4de33697afa5ab050459
brunoscarpari <brunoscarpari32@gmail.com>
2024-07-25 13:48:27 -0300

    Create objects.js

---
+++ aula-04/objects.js
@@ -0,0 +1,21 @@
+// OBJECTS
+
+const pessoa = {
+    nome: "bruno",
+    idade: 21,
+    altura: 1.83,
+    programador: true,
+    hobbies: ["carros", "academia","ler"]
+}
+
+
+
+//adicionar propriedade
+
+pessoa.profissao="desenvolvedor"
+
+//deletar uma propriedade
+
+delete pessoa.hobbies
+
+console.log(pessoa)    
